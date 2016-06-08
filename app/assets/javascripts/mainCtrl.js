d4713a5cf7d2266e37a0b720757d6deaefd8f66b
michal <mljc92@gmail.com>
2016-06-08 20:32:58 +0200

    Change indentation in mainCtrl

--- app/assets/javascripts/mainCtrl.js
+++ app/assets/javascripts/mainCtrl.js
@@ -1,17 +1,15 @@
-angular.module('orderingSystem', [])
-    .controller('mainCtrl',
-        function($scope, Order) {
-            $scope.allOrders = Order.index();
-            $scope.currentOrders = $scope.allOrders.active;
-            $scope.currentOrder = $scope.currentOrders[1];
+angular.module('orderingSystem', []).controller('mainCtrl', function($scope, Order) {
+        $scope.allOrders = Order.index();
+        $scope.currentOrders = $scope.allOrders.active;
+        $scope.currentOrder = $scope.currentOrders[1];
 
-            // Display active orders tab in the beginning.
-            $scope.ordersTabType = 'active';
+        // Display active orders tab in the beginning.
+        $scope.ordersTabType = 'active';
 
-            // Toggle between active and history orders.
-            $scope.toggleOrdersTabs = function(type) {
-                $scope.currentOrders = $scope.allOrders[type];
-                $scope.ordersTabType = type;
-            }
+        // Toggle between active and history orders.
+        $scope.toggleOrdersTabs = function(type) {
+            $scope.currentOrders = $scope.allOrders[type];
+            $scope.ordersTabType = type;
         }
-    );
+    }
+);
