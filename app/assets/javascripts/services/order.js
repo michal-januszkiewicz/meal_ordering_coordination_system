369c65a82594245011721bd06d2a45c6c46f0598
michal <mljc92@gmail.com>
2016-06-09 13:01:14 +0200

    Change orders mocked data to have orderID and change meal names and prices so you can see the difference when showing different orders

--- app/assets/javascripts/services/order.js
+++ app/assets/javascripts/services/order.js
@@ -1,37 +1,37 @@
 angular.module('orderingSystem').service('Order', function() {
     this.index = function() {
         return {
-            active : [
-                {
+            active : {
+                1: {
                     restaurant: 'restaurant2',
                     status: 'ordered',
                     initiator : 'user1',
                     meals : [
                         {user: 'user1', name: 'meal1', price: '11.00'},
-                        {user: 'user2', name: 'meal2', price: '12.00'},
-                        {user: 'user3', name: 'meal3', price: '13.00'},
+                        {user: 'user2', name: 'meal4', price: '12.00'},
+                        {user: 'user3', name: 'meal5', price: '13.00'},
                     ]},
-                {
+                2: {
                     restaurant: 'restaurant1',
                     status: 'delivered',
                     initiator : 'user2',
                     meals : [
                         {user: 'user1', name: 'meal1', price: '11.00'},
-                        {user: 'user2', name: 'meal2', price: '12.00'},
-                        {user: 'user3', name: 'meal3', price: '13.00'},
+                        {user: 'user2', name: 'meal2', price: '122.00'},
+                        {user: 'user3', name: 'meal3', price: '133.00'},
                     ]},
-                {
+                3: {
                     restaurant: 'restaurant3',
                     status: 'in progress',
                     initiator : 'user1',
                     meals : [
-                        {user: 'user1', name: 'meal1', price: '11.00'},
-                        {user: 'user2', name: 'meal2', price: '12.00'},
-                        {user: 'user3', name: 'meal3', price: '13.00'},
+                        {user: 'user1', name: 'meal1', price: '1.00'},
+                        {user: 'user2', name: 'meal22', price: '9.00'},
+                        {user: 'user3', name: 'meal32', price: '13.00'},
                     ]},
-            ],
-            history : [
-                {
+            },
+            history : {
+                4: {
                     restaurant: 'restaurant2',
                     status: 'finalized',
                     initiator : 'user1',
@@ -40,25 +40,25 @@ angular.module('orderingSystem').service('Order', function() {
                         {user: 'user2', name: 'meal2', price: '12.00'},
                         {user: 'user3', name: 'meal3', price: '13.00'},
                     ]},
-                {
+                5: {
                     restaurant: 'finalized',
                     status: 'delivered',
                     initiator : 'user2',
                     meals : [
-                        {user: 'user1', name: 'meal1', price: '11.00'},
-                        {user: 'user2', name: 'meal2', price: '12.00'},
-                        {user: 'user3', name: 'meal3', price: '13.00'},
+                        {user: 'user1', name: 'meal1234', price: '15.00'},
+                        {user: 'user2', name: 'meal223', price: '12.00'},
+                        {user: 'user3', name: 'meal323', price: '8.00'},
                     ]},
-                {
+                6: {
                     restaurant: 'restaurant3',
                     status: 'finalized',
                     initiator : 'user1',
                     meals : [
-                        {user: 'user1', name: 'meal1', price: '11.00'},
-                        {user: 'user2', name: 'meal2', price: '12.00'},
-                        {user: 'user3', name: 'meal3', price: '13.00'},
+                        {user: 'user1', name: 'meal134', price: '11.00'},
+                        {user: 'user2', name: 'meal23', price: '1222.00'},
+                        {user: 'user3', name: 'meal32', price: '13.00'},
                     ]},
-            ],
+            },
         };
     }
 });
