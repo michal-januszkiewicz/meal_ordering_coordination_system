e628e7afa72c5cde6158fd2c54fd865109047c96
michal <mljc92@gmail.com>
2016-06-11 10:30:30 +0200

    Add facebook authentication

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass'
 gem 'angular-rails-templates'
+gem 'omniauth-facebook'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'application#angular'
 
+  match "/auth/:provider/callback", to: "sessions#create", via: :all
+  match "/signout" => "sessions#destroy", as: :signout, via: :all
 end
