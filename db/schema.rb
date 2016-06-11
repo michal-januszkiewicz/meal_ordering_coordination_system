c114cf9a0db6d4119b625546224deb8a40272605
michal <mljc92@gmail.com>
2016-06-11 14:48:26 +0200

    Add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610123937) do
+ActiveRecord::Schema.define(version: 20160611124741) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "restaurant"
+    t.string   "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
