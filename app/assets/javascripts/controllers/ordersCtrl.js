564706b82f048ccacba1b80dd603bbc29f10c66f
michal <mljc92@gmail.com>
2016-06-12 13:37:06 +0200

    Reorganize ordersCtrl for more transparency

--- app/assets/javascripts/controllers/ordersCtrl.js
+++ app/assets/javascripts/controllers/ordersCtrl.js
@@ -4,47 +4,6 @@ angular.module('orderingSystem').controller('ordersCtrl', function($scope, Order
         active: {},
         history: {},
     };
-    
-    getUsers();
-    getOrders('active');
-
-    function getUsers() {
-        User.index()
-            .success(function(users) {
-                users.forEach(function(user) {
-                    $scope.users[user.id] = user;
-                });
-            });
-    }
-
-    function getOrders(type) {
-        $scope.allOrders[type] = {};
-        Order.index({type: type})
-            .success(function(orders) {
-                orders.forEach(function(order) {
-                    $scope.allOrders[type][order.id] = order;
-                });
-            });
-    }
-
-    function onOrderCreateSuccess(order) {
-        $scope.currentOrders[order.data.id] = order.data;
-    }
-
-    function onOrderCreateError(error) {
-        // Display error.
-    }
-    
-    function onMealCreateSuccess(meal) {
-        $scope.currentOrder.meals.push(meal.data);
-    }
-
-    function onMealCreateError(error) {
-        // Display error.
-    }
-
-    $scope.currentOrders = $scope.allOrders.active;
-    $scope.currentOrder = '';
     $scope.newMeal = {
         order_id: '',
         name: '',
@@ -53,6 +12,12 @@ angular.module('orderingSystem').controller('ordersCtrl', function($scope, Order
     $scope.newOrder = {
         restaurant: '',
     };
+    $scope.currentOrder = '';
+    
+    getUsers();
+    getOrders('active');
+
+    $scope.currentOrders = $scope.allOrders.active;
 
     // Display active orders tab in the beginning.
     $scope.ordersTabType = 'active';
@@ -86,4 +51,39 @@ angular.module('orderingSystem').controller('ordersCtrl', function($scope, Order
     $scope.addNewOrder = function() {
         Order.create($scope.newOrder).then(onOrderCreateSuccess, onOrderCreateError);
     };
+    
+    function getUsers() {
+        User.index()
+            .success(function(users) {
+                users.forEach(function(user) {
+                    $scope.users[user.id] = user;
+                });
+            });
+    }
+
+    function getOrders(type) {
+        $scope.allOrders[type] = {};
+        Order.index({type: type})
+            .success(function(orders) {
+                orders.forEach(function(order) {
+                    $scope.allOrders[type][order.id] = order;
+                });
+            });
+    }
+
+    function onOrderCreateSuccess(order) {
+        $scope.currentOrders[order.data.id] = order.data;
+    }
+
+    function onOrderCreateError(error) {
+        // Display error.
+    }
+
+    function onMealCreateSuccess(meal) {
+        $scope.currentOrder.meals.push(meal.data);
+    }
+
+    function onMealCreateError(error) {
+        // Display error.
+    }
 });
