c52c13dc50dc131e62745a489ef8a7fdc5cf71ef
michal <mljc92@gmail.com>
2016-06-22 14:52:12 +0200

    Authenticate each api call with auth token - backend

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,23 @@
 class Api::V1::BaseController < ApplicationController
-  # Disables CSRF protection
+  # Disable CSRF protection
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_api_with_token!
+  helper_method :current_user
+
+  def authenticate_api_with_token!
+    authenticate_or_request_with_http_token do |token, options|
+      user = User.find_by(auth_token: token)
+      if user
+        @current_user = user
+      else
+        fail 'Invalid api key'
+      end
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
