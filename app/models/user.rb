bda63af51c0602637abfb2a42f81ec2ae5e49162
michal <mljc92@gmail.com>
2016-06-22 14:36:51 +0200

    Generate user's api key in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,20 @@ class User < ActiveRecord::Base
       user.name = auth["info"]["name"]
     end
   end
+
+  def set_api_key
+    set_auth_token
+  end
+
+  private
+
+  def set_auth_token
+    return if auth_token.present?
+    self.auth_token = generate_auth_token
+    self.save
+  end
+
+  def generate_auth_token
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
 end
