dc551f15627f376adb2cbd58ad423ff090827a27
michal <mljc92@gmail.com>
2016-06-22 14:39:55 +0200

    Destroy api key when user logs out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
-class SessionsController < ActionController::Base
+class SessionsController < ApplicationController
+
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to '/#/orders'
   end
 
   def destroy
+    current_user.auth_token = nil
+    current_user.save
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Signed out!"
+    redirect_to root_url
   end
 end
