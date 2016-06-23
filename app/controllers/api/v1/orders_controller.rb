8fc5a8be3ea8331b64e1bb4f76b59b7c6061e3ad
michal <mljc92@gmail.com>
2016-06-23 13:05:31 +0200

    Refactor orders controller for more transparency

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::OrdersController < Api::V1::BaseController
+
   def index
     orders = Order.all
     orders = orders.map do |order|
@@ -12,52 +13,61 @@ class Api::V1::OrdersController < Api::V1::BaseController
     render json: orders[index_params[:type].to_sym], status: 200
   end
 
+
   def create
-    order = current_user.orders.build(order_params)
+    order = current_user.orders.build(create_params)
     order.status = 'in progress'
     if order.save!
-      render json: ::V1::OrderRepresenter.new(order).basic, status: 201
+      response = ::V1::OrderRepresenter.new(order).basic
+      status = 201
     else
-      render json: {
-          msg: 'Create order failed'
-      }, status: 500
+      response = { msg: 'Create order failed' }
+      status = 500
     end
+    render json: response, status: status
   end
+
 
   def update
     order = Order.find(params[:id])
     if order.update_attributes(update_params)
-      render json: ::V1::OrderRepresenter.new(order).basic, status: 200
+      response = ::V1::OrderRepresenter.new(order).basic
+      status = 200
     else
-      render json: {
-          msg: 'Update order failed'
-      }, status: 500
+      response = { msg: 'Update order failed' }
+      status = 500
     end
+    render json: response, status: status
   end
+
 
   def destroy
     order = Order.find(params[:id])
     if order.destroy!
-      render json: {}, status: 200
+      response = { msg: 'Order destroyed' }
+      status = 200
     else
-      render json: {
-          msg: 'Destroy order failed'
-      }, status: 500
+      response = { msg: 'Destroy order failed' }
+      status = 500
     end
+    render json: response, status: status
   end
 
 
   private
 
-  def order_params
+  def create_params
     params.permit(:restaurant)
   end
+
 
   def index_params
     params.permit(:type)
   end
 
+
   def update_params
     params.permit(:restaurant, :status)
   end
+
 end
