1fb8130987e015b49e50c6ea07a02b9930595277
michal <mljc92@gmail.com>
2016-06-24 12:29:43 +0200

    Add seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Seeds: start'
+
+5.times do |i|
+  User.create!(
+          name: "User #{i+1}"
+  )
+end
+
+3.times do
+  Order.create!(
+           restaurant: "restaurant #{rand(1..10)}",
+           status: 'finalized'
+  )
+end
+
+Order.create!(
+    restaurant: "restaurant #{rand(1..10)}",
+    status: 'in progress'
+)
+
+Order.create!(
+    restaurant: "restaurant #{rand(1..10)}",
+    status: 'ordered'
+)
+
+Order.create!(
+    restaurant: "restaurant #{rand(1..10)}",
+    status: 'delivered'
+)
+
+meals = Array.new
+10.times do |i|
+  meals.push(
+      {
+          name: "meal #{i+1}",
+          price: rand(10..20).to_f + rand(0..99)/100.to_f,
+      }
+  )
+end
+
+Order.all.each do |order|
+  rand(1..5).times do |i|
+    meal = meals[rand(1..10)]
+    meal['user_id'] = i +1
+    meal['order_id'] = order.id
+    order.meals << Meal.create!(meal)
+  end
+end
+
+puts 'Seeds: done'
+
