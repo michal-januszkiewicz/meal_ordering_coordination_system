dd4fb50d3f006c38256d3fdf6ac31701c3a78ca0
michal <mljc92@gmail.com>
2016-06-25 14:37:58 +0200

    Enable debug mode on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,6 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.assets.debug = true
-
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
