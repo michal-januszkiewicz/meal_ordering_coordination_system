612498a27f830bad386a7d65143f5e8172b832aa
michal <mljc92@gmail.com>
2016-06-25 20:29:53 +0200

    Add email to users model to send notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615174505) do
+ActiveRecord::Schema.define(version: 20160625182333) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "meals", force: :cascade do |t|
     t.string   "name"
@@ -22,8 +25,8 @@ ActiveRecord::Schema.define(version: 20160615174505) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "meals", ["order_id"], name: "index_meals_on_order_id"
-  add_index "meals", ["user_id"], name: "index_meals_on_user_id"
+  add_index "meals", ["order_id"], name: "index_meals_on_order_id", using: :btree
+  add_index "meals", ["user_id"], name: "index_meals_on_user_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.string   "restaurant"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160615174505) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -42,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160615174505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "auth_token"
+    t.string   "email"
   end
 
 end
